9b3a2f7b7f5736827d98e5bc7a11b49485992701
Ronak <ronak@promact.com>
2016-07-01 10:10:50 +0530

    .Net Core First Three Demo Project

---
+++ MvcMovie/src/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 5)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+        //First only Controller added no view and no model
+
+
+        //public ActionResult Index()
+        //{
+        //    //return "This is my default action...";
+        //    return View();
+        //}
+
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        //public string Welcome()
+        //{
+        //    return "This is the Welcome action method...";
+        //}
+
+        //public string welcomeWithParameter(string name, int numTimes = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, id: {numTimes}");
+        //}
+    }
+}
---
+++ TodoApi/src/TodoApi/Models/TodoRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoRepository:ITodoRepository
+    {
+        //To add System.Collections.Concurrent;
+        private static ConcurrentDictionary<string, TodoItem> _todos =
+              new ConcurrentDictionary<string, TodoItem>();
+        public TodoRepository()
+        {
+            Add(new TodoItem { Name = "Item1" });
+        }
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _todos.Values;
+        }
+        public void Add(TodoItem item)
+        {
+            item.key = Guid.NewGuid().ToString();
+            _todos[item.key] = item;
+        }
+        public TodoItem Find(string key)
+        {
+            TodoItem item;
+            _todos.TryGetValue(key, out item);
+            return item;
+        }
+
+        public TodoItem Remove(string key)
+        {
+            TodoItem item;
+            _todos.TryGetValue(key, out item);
+            _todos.TryRemove(key, out item);
+            return item;
+        }
+
+        public void Update(TodoItem item)
+        {
+            _todos[item.key] = item;
+        }
+
+    }
+}
